8601e66a1a90b7b192d45b2e1b0fd879f350a1ac
Veronica Flores <veronicaflores@Veronicas-MacBook-Pro.local>
2023-09-13 21:13:14 +0300

    Assignment Complete

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -1,13 +1,29 @@
 package com.example.Bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.Bookstore.model.Book;
+import com.example.Bookstore.model.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			Book b1 = new Book("A Farewell to Arms", "Ernest Hemingway", 1929, "1232323-21", 49.99);
+			Book b2 = new Book("Animal Farm", "George Orwell", 1945, "2212343-5", 39.99);
+			
+			repository.save(b1);
+			repository.save(b2);
+		};
 	}
 
 }
--- src/main/java/com/example/Bookstore/model/Book.java
+++ src/main/java/com/example/Bookstore/model/Book.java
@@ -1,12 +1,40 @@
 package com.example.Bookstore.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
     private String title;
     private String author;
     private int publicationYear;
     private String isbn;
     private double price;
     
+    public Book() {}
+    
+	public Book(String title, String author, int publicationYear, String isbn, double price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.publicationYear = publicationYear;
+		this.isbn = isbn;
+		this.price = price;
+	}
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
 	public String getTitle() {
 		return title;
 	}
